83bb2dc4f3751c141d5eeeb36edd0fd9551c7863
leeiter <jesgipne@hanmail.net>
2019-10-15 16:44:17 +0900

    2019-10-15

---
+++ JDBC_01_Hello/src/com/biz/jdbc/exec/JdbcEx_03.java
@@ -0,0 +1,23 @@
+package com.biz.jdbc.exec;
+
+import java.util.List;
+
+import com.biz.jdbc.domain.ScoreVO;
+import com.biz.jdbc.service.ScoreJDBCServiceV2;
+
+public class JdbcEx_03 {
+
+	public static void main(String[] args) {
+		
+		ScoreJDBCServiceV2 sc = new ScoreJDBCServiceV2();
+		
+		sc.selectAll();
+		List<ScoreVO> scoreList = sc.gerScoreListr();
+		
+		for(ScoreVO scoreVO : scoreList) {
+			System.out.println(scoreVO.toString());
+		}
+
+	}
+
+}
